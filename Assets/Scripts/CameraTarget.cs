902eebc2576cf0a0ad97f5c9335960a33cdac781
tailow <tailows@outlook.com>
2024-04-13 17:15:03 +0300

    CameraStuff prefab and new input system

---
+++ Assets/Scripts/CameraTarget.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CameraTarget : MonoBehaviour
+{
+    private Camera _mainCamera;
+
+    private void Start()
+    {
+        _mainCamera = Camera.main;
+    }
+}
