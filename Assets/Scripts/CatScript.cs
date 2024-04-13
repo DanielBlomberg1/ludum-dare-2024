632c8cc67380dc3b90cc0a3c2e91cab864e7d57f
DAN-KOTI\Daniel <daniel.blombergof@gmail.com>
2024-04-13 11:43:59 +0300

    add dev cat prefab and few test levels

---
+++ Assets/Scripts/CatScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatScript : MonoBehaviour
+{
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector2(1, rb.velocity.y);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Flag")
+        {
+           Destroy(gameObject);
+        }
+    }
+}
