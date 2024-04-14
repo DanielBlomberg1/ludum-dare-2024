08ff961c9c2789ec47213664b647f3feb368b174
DAN-KOTI\Daniel <daniel.blombergof@gmail.com>
2024-04-14 14:10:48 +0300

    Add giftbox prefab for adding items to player

---
+++ Assets/Scripts/GiftBoxScript.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GiftBoxScript : MonoBehaviour
+{
+    [SerializeField] private SummonItem giftItem;
+    
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Cat"))
+        {
+            InventoryManager.Instance.AddItem(giftItem);
+            Destroy(gameObject);
+        }
+    }
+}
