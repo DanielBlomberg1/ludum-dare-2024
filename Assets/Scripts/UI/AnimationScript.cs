d4c6668d95d71b9e6eb54e63d69f94eab3e8bd12
DAN-KOTI\Daniel <daniel.blombergof@gmail.com>
2024-04-14 21:54:53 +0300

    add meme soundeffects to cutscene

--- Assets/Scripts/UI/AnimationScript.cs
+++ Assets/Scripts/UI/AnimationScript.cs
@@ -12,6 +12,9 @@ public class AnimationScript : MonoBehaviour
     [SerializeField] private GameObject fire;
     [SerializeField] private Sprite DevilCat;
     [SerializeField] private GameObject textPart; 
+    [SerializeField] private AudioClip[] clips;
+
+    private AudioSource aS;
 
     private Image catImg;
     private Image fireImg;
@@ -25,21 +28,34 @@ public class AnimationScript : MonoBehaviour
     {
         catImg = cat.GetComponent<Image>();
         fireImg = fire.GetComponent<Image>();
+        aS = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
         if(currPhase == 0){
+            
             // move cat towards its position but the x position is 0
             // cat is an ui object with a rect trsnform
-            cat.transform.position = Vector3.MoveTowards(cat.transform.position, new Vector3(Screen.width/2, cat.transform.position.y, cat.transform.position.z), 0.1f);
+            cat.transform.position = Vector3.MoveTowards(cat.transform.position, new Vector3(Screen.width/2, cat.transform.position.y, cat.transform.position.z), 0.15f);
+            
+            //play the first sound
+            if(!aS.isPlaying){
+                aS.clip = clips[0];
+                aS.Play();
+            }
             if(cat.transform.position.x == Screen.width/2) currPhase++;
         }else if(currPhase == 1){
             maxTime = 5.0f;
             timer += Time.deltaTime;
             if(catImg.sprite != cat2){
                 catImg.sprite = cat2;
+                //play the second sound
+                if(clips[1] != null){
+                    aS.clip = clips[1];
+                    aS.Play();
+                }
             }
             if(timer >= maxTime){
                 currPhase++;
@@ -51,6 +67,10 @@ public class AnimationScript : MonoBehaviour
             if(catImg != cat3){
                 catImg.sprite = cat3;
                 fire.SetActive(true);
+                if(clips[2] != null){
+                    aS.clip = clips[2];
+                    aS.Play();
+                }
             }
             if(timer >= maxTime){
                 currPhase++;
@@ -62,6 +82,10 @@ public class AnimationScript : MonoBehaviour
             if(fireImg != DevilCat){
                 fireImg.sprite = DevilCat;
                 catImg.sprite = cat4;
+                if(clips[3] != null){
+                    aS.clip = clips[3];
+                    aS.Play();
+                }
             }
             if(timer >= maxTime){
                 currPhase++;
