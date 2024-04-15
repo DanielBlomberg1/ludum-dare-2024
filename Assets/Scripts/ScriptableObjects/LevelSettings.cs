fa21278c49a292367c3be5c1dd6b3fe73cc76e65
DAN-KOTI\Daniel <daniel.blombergof@gmail.com>
2024-04-15 19:00:37 +0300

    cat walks backwards also

--- Assets/Scripts/ScriptableObjects/LevelSettings.cs
+++ Assets/Scripts/ScriptableObjects/LevelSettings.cs
@@ -36,5 +36,8 @@ public class LevelSettings : ScriptableObject
     public float spawnDelay { get; private set; } = 5.0f;
 
     [field: SerializeField]
+    public float CatSpeed { get; private set; } = 1;
+
+    [field: SerializeField]
     public bool isDark { get; private set; } = false;
 }
