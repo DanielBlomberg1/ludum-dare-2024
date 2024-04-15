d6f8c2235c6041754e7edae8c1693da7b55325c8
Mikael Björke <contact@mbjorke.com>
2024-04-16 02:24:46 +0300

    Polish in-game UI

--- Assets/Scripts/UI/UiTextScript.cs
+++ Assets/Scripts/UI/UiTextScript.cs
@@ -8,12 +8,14 @@ public class UiTextScript : Singleton<UiTextScript>
     [SerializeField] private TMP_Text catsEdged;
 
     private int maxCats;
+    private int requiredAmount;
 
     private void Start() {
         catsOutOfMaxx.color = Color.white;
         maxCats = GameManager.Instance.GetCurrentLevelSettings().CatAmount;
+        requiredAmount = GameManager.Instance.GetCurrentLevelSettings().CatsRequiredToPass;
         UpdateCatsOutOfMaxx(0);
-        UpdateRequiredText(GameManager.Instance.GetCurrentLevelSettings().CatsRequiredToPass);
+        UpdateRequiredText(maxCats);
     }
 
     public void UpdateRequiredText(int required)
@@ -23,7 +25,7 @@ public class UiTextScript : Singleton<UiTextScript>
 
     public void UpdateCatsOutOfMaxx(int cats)
     {
-        catsOutOfMaxx.text = $"{cats}/{maxCats}";
+        catsOutOfMaxx.text = $"{cats}/{requiredAmount}";
     }
 
     public void UpdateCatsEdged(int cats)
